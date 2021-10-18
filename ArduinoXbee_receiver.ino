c39860a18df9ec5a07e0a2e754c3cd9bfdca6f12
Haolat3 <92693957+Haolat3@users.noreply.github.com>
2021-10-18 12:33:33 -0400

    Update ArduinoXbee_receiver.ino

--- ArduinoXbee_receiver.ino
+++ ArduinoXbee_receiver.ino
@@ -1,18 +1,31 @@
 #include <SoftwareSerial.h>
+#include <XBee.h>
+
 SoftwareSerial XBee(2, 3);
+Xbee xbee = XBee();
+int LED_Output = 28; //LED output is pin 28
+int Data_State = 0;// State of the data
+
+//This is the Arduino reciver. This arduino has a LED that
+//turn on when data is received from the transmitter arduino
+
 
 void setup()
 {
-  XBee.begin(9600);
+  pinMode(LED_Output, OUTPUT);
+  xbee.begin(9600);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if (Serial.available()){ 
-    XBee.write(Serial.read());
-  }
-  if (XBee.available()) { 
-    Serial.write(XBee.read());
+  Data_State = Xbee.read(); // read data transmitted from transmitter arduinos
+  //if data was received turn on the LED
+  if (Data_State != 0) {
+    digitalWrite(LED_Output, HIGH);
+    delay(200);// wait for a second
+  } else {
+    digitalWrite(LED_Output, LOW);
+    delay(200);
   }
 }
