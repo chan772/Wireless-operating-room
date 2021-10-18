3257c89dc260858aa6e244bff0c4df1c1d206caa
Haolat3 <92693957+Haolat3@users.noreply.github.com>
2021-10-18 12:50:40 -0400

    Update arduino_receiver.ino

--- arduino_receiver.ino
+++ arduino_receiver.ino
@@ -1,6 +1,5 @@
-#include <SoftwareSerial.h>
+#include <Wire.h>
 
-SoftwareSerial XBee(2, 3);
 int LED_Output = 28; //LED output is pin 28
 int Data_State = 0;// State of the data
 
@@ -8,16 +7,15 @@ int Data_State = 0;// State of the data
 //turn on when data is received from the transmitter arduino
 
 
-void setup()
-{
+void setup() {
   pinMode(LED_Output, OUTPUT);
-  XBee.begin(9600);
+  Wire.begin(9); // join i2c bus (address optional for master)
+  Wire.onReceive(receiveEvent);
   Serial.begin(9600);
 }
 
-void loop()
-{
-  Data_State = XBee.read(); // read data transmitted from transmitter arduinos
+// the loop function runs over and over again forever
+void loop() {
   //if data was received turn on the LED
   if (Data_State != 0) {
     digitalWrite(LED_Output, HIGH);
@@ -26,4 +24,9 @@ void loop()
     digitalWrite(LED_Output, LOW);
     delay(200);
   }
+}
+
+
+void receiveEvent(int howMany) {
+  Data_State = Wire.read(); // read data transmitted from transmitter arduino
 }
