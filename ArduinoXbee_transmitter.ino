43bb7886447590149bfef396dbc2e96402725598
Haolat3 <92693957+Haolat3@users.noreply.github.com>
2021-10-18 12:27:03 -0400

    Update ArduinoXbee_transmitter.ino

--- ArduinoXbee_transmitter.ino
+++ ArduinoXbee_transmitter.ino
@@ -1,18 +1,33 @@
 #include <SoftwareSerial.h>
+#include <XBee.h>
+
 SoftwareSerial XBee(2, 3);
+int Pulse_Sensor = A2;//Pulse Senor input is pin A2
+int Push_button = 26; //push button that starts data collection from pulse sensor is at pin 2
+int Pulse_data;
+Xbee xbee = XBee();
+
+//This is the Arduino that transmits data. This arduino has a pulse sensor and switch.
+//When the switch is pressed the ardunino collects data fom the pulse sensor.
+//Then the data is tansmitted through wire to the receiving Arduino.
+
 
 void setup()
 {
-  XBee.begin(9600);
+  pinMode(Pulse_Sensor, OUTPUT);
+  pinMode(Push_button, INPUT);
+  xbee.begin(9600);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if (Serial.available()){ 
-    XBee.write(Serial.read());
+  if (digitalRead(Push_button) == HIGH) { 
+    Pulse_data = analogRead(Pulse_Sensor);
   }
-  if (XBee.available()) { 
-    Serial.write(XBee.read());
+  else {
+    Pulse_data = 0;
   }
+  xbee.write(Pulse_data);
+  delay(200);
 }
